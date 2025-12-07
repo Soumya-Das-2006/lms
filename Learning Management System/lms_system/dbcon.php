90956a25f1274002da86e47b9d398266d004f075
Soumya-Das-2006 <soumyadastopper2006l@example.com>
2025-12-07 21:25:56 +0530

    Updated project structure and files

--- Learning Management System/lms_system/dbcon.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost','root','','parul_university_lms') or die(mysqli_error());
-
-?>
--- Learning Management System/student_classes/previous_class.php
+++
@@ -1,2 +0,0 @@
-Make a previous class which class is take by teacher all class student can see previous class and download the class video and notes.
-Path: previous_class.php
